7c8ed5751eb67549841ec21279940d8cf9bab57c
zwj <91038485+zwj1116@users.noreply.github.com>
2022-05-01 20:40:02 +0800

    Modified and debugged find.py

--- lmpthw/ex6_find/find.py
+++ lmpthw/ex6_find/find.py
@@ -1,2 +1,15 @@
 import os, argparse
 parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
+
+parser.add_argument("path", metavar="P", type=str, help="Enter a path to find.")
+parser.add_argument("filename", metavar="F", type=str, help="Enter a filename to find.")
+args = parser.parse_args()
+
+for root,dirs,files in os.walk(args.path,topdown=True):
+    if args.filename in files:
+        print("found it.")
+        print(root)
+        break
+    else:
+        pass
