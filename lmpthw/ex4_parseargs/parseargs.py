99715e9f8d87a1ef33ef304359194ce8c6acb4b5
zwj <91038485+zwj1116@users.noreply.github.com>
2022-05-01 20:25:41 +0800

    Update parseargs.py

--- lmpthw/ex4_parseargs/parseargs.py
+++ lmpthw/ex4_parseargs/parseargs.py
@@ -1,8 +1,10 @@
-# my solution
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f","--foo",help="foo")
-parser.add_argument("-b","--bar",help="bar")
-parser.add_argument("-v","--version",help="Print program's version")
-parser.parse_args()
+parser.add_argument("interger", type=str, help="Enter a interger to print")
+parser.add_argument("-f", help="foo")
+parser.add_argument("-b", help="bar")
+
+args = parser.parse_args()
+
+print(args)
